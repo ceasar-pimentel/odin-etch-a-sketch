022d58404b257ca3ee3565c241f3ca1f895e7613
ceasarpimentel <privateemailused@noreply.github.com>
2024-10-07 19:51:45 -0700

    feat: finished all javascript logic for project

---
+++ javascript.js
@@ -0,0 +1,151 @@
+// in this project you have to construct the page with only javascript.
+/*
+    plan of attack
+    1. use flex box to create a square box where the height is equal to the length.]
+    2. calculate the width of the box oby dividing the full width of the container by the amount of boxes from user input
+    3. use hover event to change color of the box. 
+*/
+
+// builds the basic layout for divs that will contain the header and etch a sketch portion of the page.
+// will set variables to hold the header elemenet and main body element so document doesnt have to search from beginning 
+
+// dom objects
+let body = null;
+let container = null;
+let etchDiv = null;
+let form = null;
+let header = null;
+let input = null;
+let inputBtn = null;
+let main = null;
+
+
+// fields;
+let bodyWidth = null;
+
+//#region page layout and styling 
+
+function initializeDomObjects() {
+    body = document.querySelector("body");
+    container = document.querySelector("#container");
+    etchDiv = main = document.createElement("div");
+    form = document.createElement("form");
+    inputBtn = document.createElement("input");
+    input = document.createElement("input");
+    header = document.createElement("div");
+    main = document.createElement("div");
+}
+
+function buildBody() {
+    body.style.padding = "0px";
+    body.style.margin = "0px";
+}
+
+function buildHeader() {
+    header.style.display = "flex";
+    header.style.justifyContent = "center";
+    header.id = "header";
+
+    input.style.margin = "5px";
+    input.type = "text";
+
+    inputBtn.type = "button";
+    inputBtn.value = "draw";
+
+    form.appendChild(input);
+    form.appendChild(inputBtn);
+    header.appendChild(form);
+
+}
+
+function buildEtchASketch() {
+    etchDiv.id = "etch-div";
+    etchDiv.style.display = "flex";
+    etchDiv.style.flexWrap = "wrap";
+    etchDiv.style.justifyContent = "center";
+    etchDiv.style.alignItems = "center";
+    etchDiv.style.gap = "0px";
+    etchDiv.style.borderStyle ="solid";
+
+}  
+
+function buildLayout() {
+    buildBody();
+    buildHeader();
+    buildEtchASketch();
+
+    main.appendChild(etchDiv);
+    container.appendChild(header);
+    container.appendChild(main);
+
+    bodyWidth = window.innerWidth;
+}
+
+//#endregion
+
+//#region game logic
+
+function calculateSquareSize(numOfSquares) {
+    let squareWidth = Math.floor((bodyWidth * .50)/ numOfSquares);
+    return squareWidth;
+}
+
+function removeAllPixels() {
+    let children = Array.from(etchDiv.children);
+    children.forEach((child) => {
+        etchDiv.removeChild(child);
+    });
+    body.setAttribute("style", "padding: 16px 25%;");
+}
+
+function recalculateBodyPadding(squareSize,  numOfSquares) {
+    // updating the window width in case it might have changed
+    bodyWidth = body.getBoundingClientRect().width;
+   
+    let calculatedWidth = squareSize * numOfSquares;
+    let newCalcPadding = Math.ceil(((bodyWidth - calculatedWidth)-4)/2); 
+    console.log(`squareSize: ${squareSize} windowWidth: ${bodyWidth} calculatedWith: ${calculatedWidth} new calc padding: ${newCalcPadding}`);
+    return newCalcPadding;
+}
+
+function constructBoard() {
+
+    let num = Number(input.value);
+    
+    // removing and clearing the board in case there was prior drawing.
+    removeAllPixels();
+
+    if (Number.isNaN(num)) {
+        return;
+    }
+
+    let squareSize = calculateSquareSize(num);
+    let squares = num * num;
+
+    let newPadding = recalculateBodyPadding(squareSize, num);
+    body.style.padding = `16px ${newPadding}px`;
+
+    for (let i = 0; i < squares; i++) {
+        let pixel = document.createElement("div");
+        pixel.style.flexBasis = `${squareSize}px`;
+        pixel.style.aspectRatio = "1/1";
+        pixel.onmouseenter = (event) => { 
+            event.target.style.backgroundColor ="black"; 
+        };
+        etchDiv.appendChild(pixel);
+    }
+}
+
+//#endregion
+
+function initializePage() {
+    container = document.querySelector("#container");
+    initializeDomObjects();
+    buildLayout();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializePage();
+    inputBtn.addEventListener("click", constructBoard);
+})
+
